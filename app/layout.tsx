c1627d2bd243d119fe0d2c231cffab873fc2801e
Raymond <raymondurrutia2@yahoo.com>
2025-06-13 05:31:37 -0400

    Add header with responsive logo and nav links

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,28 @@
 import "./globals.css";
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body>
+				<header className="w-full bg-white">
+					<nav className="flex justify-between px-6 py-4">
+						<div className="relative">
+							{/* Desktop logo */}
+							<img src="/printforge-logo-d.svg" alt="PrintForge Logo" className="w-[240px] h-auto hidden md:block" />
+							<img src="/printforge-logo-m.svg" alt="PrintForge Logo" className="w-[60px] h-auto block md:hidden" />
+						</div>
+						<ul className="flex gap-6 items-center">
+							<li>3D Models</li>
+							<li>About</li>
+						</ul>
+					</nav>
+				</header>
+				{children}
+			</body>
+		</html>
+	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,30 @@
 export default function Home() {
-    return (
-        <main>
-            <section className="flex flex-col-reverse items-center justify-between gap-8 px-6 py-12 mx-auto md:flex-row max-w-7xl">
-                <div className="flex-1 space-y-6">
-                    <p className="hidden text-sm text-gray-600 uppercase md:block">
-                        Your go-to platform for 3D printing files
-                    </p>
-                    <h1 className="text-4xl font-bold md:text-5xl">
-                        Discover what's possible with 3D Printing
-                    </h1>
-                    <p className="text-lg text-gray-600">
-                        Join our community of creators and explore a vast
-                        library of user-submitted models.
-                    </p>
+  return (
+    <main>
+      <section className="flex flex-col-reverse items-center justify-between gap-8 px-6 py-12 mx-auto md:flex-row max-w-7xl">
+        <div className="flex-1 space-y-6">
+          <p className="hidden text-sm text-gray-600 uppercase md:block">
+            Your go-to platform for 3D printing files
+          </p>
+          <h1 className="text-4xl font-bold md:text-5xl">
+            Discover what's possible with 3D Printing
+          </h1>
+          <p className="text-lg text-gray-600">
+            Join our community of creators and explore a vast
+            library of user-submitted models.
+          </p>
 
-                    <div className="flex gap-4">
-                        <button
-                            href="/3d-models"
-                            className="px-6 py-3 text-white transition duration-100 bg-black border-2 border-white hover:bg-white hover:text-black hover:border-black"
-                        >
-                            Browse Models
-                        </button>
-                    </div>
-                </div>
-                <img src="/home-hero-image.png" width={350} />
-            </section>
-        </main>
-    );
+          <div className="flex gap-4">
+            <button
+              href="/3d-models"
+              className="px-6 py-3 text-white transition duration-100 bg-black border-2 border-white hover:bg-white hover:text-black hover:border-black"
+            >
+              Browse Models
+            </button>
+          </div>
+        </div>
+        <img src="/home-hero-image.png" width={350} />
+      </section>
+    </main>
+  );
 }
