759933a6acbe1b4b98aa85596f610ce6569a6f4b
Raymond <raymondurrutia2@yahoo.com>
2025-06-26 05:54:31 -0400

    Create category pages

--- app/3d-models/categories/[categoryName]/page.tsx
+++ app/3d-models/categories/[categoryName]/page.tsx
@@ -1,18 +1,25 @@
 // Import the type definition for the expected props structure of this page
 import type { CategoryPageProps } from "@/app/types"
+import ModelsGrid from "@/app/components/ModelsGrid"
 
 // Import a helper function to get full category data by its slug that's used in the URL
 import { getCategoryBySlug } from "@/app/lib/categories"
+import { getModels } from "@/app/lib/models"
 
 // Dynamic route component for pages like /categories/[categoryName]
 export default async function CategoryPage({ params }: CategoryPageProps) {
+
   // Params is a Promise here to simulate future async setup (e.g., when using a database)
   const { categoryName } = await params
 
   // Get category data from local JSON based on the URL slug (will be replaced with async DB call later)
   const category = getCategoryBySlug(categoryName)
-  console.log(category)
 
-  // Display the category's human-readable name
-  return <h1>{category.displayName}</h1>
+  // Get the models that belong to specified category name
+
+  const models = await getModels({ category: categoryName })
+
+  return (
+    <ModelsGrid title={category.displayName} models={models} />
+  )
 }
--- app/components/NavLink.tsx
+++ app/components/NavLink.tsx
@@ -3,10 +3,6 @@ import type { NavLinkProps } from "@/app/types"
 
 export default function NavLink({ href, isActive, children }: NavLinkProps) {
   return (
-    <>
-      <li className="text-sm uppercase list-none">
-        <Link href={href} className={`px-4 py-2 transition-colors rounded-md cursor-pointer hover:text-orange-400 ${isActive ? "text-orange-400" : "text-gray-700"}`}>{children}</Link>
-      </li>
-    </>
+    <Link href={href} className={`px-4 py-2 transition-colors rounded-md cursor-pointer hover:text-orange-400 ${isActive ? "text-orange-400" : "text-gray-700"}`}>{children}</Link>
   )
 }
