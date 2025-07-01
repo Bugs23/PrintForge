7ec845d259f17b6ac1849a9a007a99f4fe3a26d3
Raymond <raymondurrutia2@yahoo.com>
2025-07-01 06:51:38 -0400

    Update 3d-models page  form to next.js Form component

--- app/3d-models/page.tsx
+++ app/3d-models/page.tsx
@@ -1,3 +1,4 @@
+import Form from "next/form"
 import ModelsGrid from "@/app/components/ModelsGrid"
 import { getModels } from "@/app/lib/models"
 import type { ModelPageProps } from "@/app/types"
@@ -19,7 +20,7 @@ export default async function Page({ searchParams }: ModelPageProps) {
 
   return (
     <div>
-      <form className="w-full px-5 md:px-0 md:max-w-xl">
+      <Form action="/3d-models" className="w-full px-5 md:px-0 md:max-w-xl">
         <input
           type="text"
           name="query"
@@ -28,7 +29,7 @@ export default async function Page({ searchParams }: ModelPageProps) {
           defaultValue={query}
           className="w-full py-3 pl-5 pr-5 text-sm placeholder-gray-500 bg-white border border-[#606060] rounded-full focus:border-[#606060] focus:outline-none focus:ring-0 md:text-base"
         />
-      </form>
+      </Form>
       {
         // If there are models that match the word the user searched for, display them in the models grid
         // If there are no models that match the word the user searched for, display a message
